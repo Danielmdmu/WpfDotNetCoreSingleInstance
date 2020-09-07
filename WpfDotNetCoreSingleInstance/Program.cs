2207ce9dc507a1273fbac0ae22b4dff9af11b02d
Daniel Müller <daniel.mueller@docufy.de>
2020-09-07 21:01:09 +0200

    Changed Startup-Method to Main() in Program.cs

---
+++ WpfDotNetCoreSingleInstance/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WpfDotNetCoreSingleInstance
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+
+            app.Run(new MainWindow());
+        }
+    }
+}
